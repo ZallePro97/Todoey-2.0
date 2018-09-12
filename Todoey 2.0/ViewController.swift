9a90beff8ab88d23866f5f88e23579b7df41a53e
Zeljko Halle <Zeljko@zeljkos-macbook-air.local>
2018-09-12 11:18:33 +0200

    Initial Commit

---
+++ Todoey 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey 2.0
+//
+//  Created by Zeljko Halle on 12/09/2018.
+//  Copyright © 2018 Zeljko Halle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
